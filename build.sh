aec9c6af1ccd1df6a4f829302ed903fbb26702ee
Sean Liao <seankhliao@gmail.com>
2019-11-12 21:52:19 +0100

    add a lot

--- build.sh
+++ build.sh
@@ -12,19 +12,28 @@ EXT=pkg.tar.zst
 mkdir $BUILD
 HOME=/tmp
 yay -S --noconfirm --builddir $BUILD --batchinstall \
+    dockerfile-language-server-bin \
     downgrade \
     exa-git \
     go-git \
     google-chrome-dev \
     google-chrome \
+    javascript-typescript-langserver \
     kitty-git \
     kubernetes-helm-git \
+    microsoft-python-language-server \
     neovim-git \
     neovim-plug-git \
     ripgrep-git \
     rsync-git \
     tag-ag \
+    texlab-git \
+    vscode-css-languageserver-bin \
+    vscode-html-languageserver-bin \
+    vscode-json-languageserver-bin \
     wl-clipboard-x11 \
+    xml-language-server-git \
+    yaml-language-server-bin \
     yay-bin \
     zsh-completions-git
 
